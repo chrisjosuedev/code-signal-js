90aebcd8f8d3c610d34e649b4ad2b3a46d649418
Cristhian Martínez <chrismartinezlara@outlook.es>
2022-09-10 22:17:00 -0600

    New Common Character Solution Pro

--- 10-commonCharacterCount.js
+++ 10-commonCharacterCount.js
@@ -2,6 +2,9 @@
  * Given two strings, find the number of common characters between them.
  */
 
+/*** SOLUTION #1 */
+
+/** No Functions Method */
 function solution(s1, s2) {
     const alreadyIncluded = [];
     let count = 0;
@@ -27,4 +30,22 @@ function amountString(string, letter) {
         if (string[i] === letter) index.push(i);
     }
     return index.length;
+}
+
+
+/*** SOLUTION #1 */
+
+/** CodeSignal User -> lifeofnations Solution */
+/** Regex and JS Methods */
+
+/** replace(value1, substring) 
+ * It will be replace every coincidence (s1 in s2) by a "!" (Could be any character.).
+ * then, with /[^!]/g regex, replace every character excepts "!", and get new string length.
+*/
+
+function solution(s1, s2) {
+    for (var i = 0; i < s1.length; i++) {
+        s2 = s2.replace(s1[i], '!');
+    }
+    return s2.replace(/[^!]/g, '').length;
 }
